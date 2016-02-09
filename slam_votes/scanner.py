24e7d0725198b0cc5a70fdee985a069f58430c85
David Schuler <schulerdavid@gmail.com>
2016-02-09 13:58:33 +0100

    fixed formatting

--- slam_votes/scanner.py
+++ slam_votes/scanner.py
@@ -8,24 +8,26 @@ import math
 
 # Debug switches
 DEBUG = False
-VISUAL_DEBUG = True 
+VISUAL_DEBUG = True
 VISUAL_DEBUG_SHOW_BLURED_AND_THRESHOLDED_IMG = False
 VISUAL_DEBUG_SHOW_ALL_LINES = False
 VISUAL_DEBUG_SHOW_BOUNDING_EDGES = False
 
+
 def displayLine(img, line):
     x1, y1, x2, y2 = line
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
- 
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+
 def getGradient(line):
     x1, y1, x2, y2 = line
     m = 0
     if x1 != x2:
-        m = (y2 - y1) / (x2 - x1) 
+        m = (y2 - y1) / (x2 - x1)
     return m
 
-def resizeAndDisplay(img, name, wait):
 
+def resizeAndDisplay(img, name, wait):
     # resize image
     new_with = 500
     # the ratio of the new image to the old image
@@ -34,6 +36,7 @@ def resizeAndDisplay(img, name, wait):
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
     cv2.imshow(name, resized)
     cv2.waitKey(wait)
+
 
 def getVotesFromImage(imageName):
     # load the image and convert it to grayscale
@@ -47,25 +50,27 @@ def getVotesFromImage(imageName):
     # blur image
     working_img = cv2.GaussianBlur(img, (15, 15), 0)
     # get black white image
-    tmp, working_img = cv2.threshold(working_img, 100, 255, 
+    tmp, working_img = cv2.threshold(
+            working_img, 100, 255,
             cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     if VISUAL_DEBUG and VISUAL_DEBUG_SHOW_ALL_LINES:
         resizeAndDisplay(working_img, "Thresh", 1)
         resizeAndDisplay(display_img, "comp", 0)
 
-    #invert image
+    # invert image
     working_img = (255 - working_img)
 
-    # detect lines in image 
-    edges = cv2.Canny(working_img, 100, 200, apertureSize = 3)
-    lines = cv2.HoughLinesP(edges, 1, numpy.pi/180, 40,
-            minLineLength = 10, maxLineGap=6)
+    # detect lines in image
+    edges = cv2.Canny(working_img, 100, 200, apertureSize=3)
+    lines = cv2.HoughLinesP(
+            edges, 1, numpy.pi/180, 40,
+            minLineLength=10, maxLineGap=6)
     if DEBUG and (lines is not None):
         print("Deteted {} lines in image".format(len(lines)))
     x_min = sys.maxsize
     x_max = 0
-    y_min = sys.maxsize 
+    y_min = sys.maxsize
     y_max = 0
     x_minline = None
     x_maxline = None
@@ -73,8 +78,8 @@ def getVotesFromImage(imageName):
     y_maxline = None
     for i in lines:
         count = 0
-        for x1,y1,x2,y2 in i:
-            #TODO if isVertical(line)
+        for x1, y1, x2, y2 in i:
+            # TODO if isVertical(line)
             if x1 > x_max or x2 > x_max:
                 x_max = max(x1, x2)
                 x_maxline = i[0]
@@ -90,7 +95,7 @@ def getVotesFromImage(imageName):
                 y_minline = i[0]
             VISUAL_DEBUG_ALL_LINES = False
             if VISUAL_DEBUG and VISUAL_DEBUG_ALL_LINES:
-                cv2.line(display_img,(x1,y1),(x2,y2),(255,0,0),2)
+                cv2.line(display_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
     if VISUAL_DEBUG:
         displayLine(display_img, x_minline)
@@ -106,49 +111,54 @@ def getVotesFromImage(imageName):
         displayLine(working_img, y_maxline)
         resizeAndDisplay(working_img, "Bounding edges in working_img", 0)
 
+    # get gradients for the bounding box
     m_vert_1 = getGradient(x_minline)
     m_vert_2 = getGradient(x_maxline)
-
     m_hor_1 = getGradient(y_minline)
     m_hor_2 = getGradient(y_maxline)
 
-    # translate images 
+    # translate images
     shiftx = 0 - x_minline[0]
     shifty = 0 - y_minline[1]
-    M = numpy.float32([[1,0, shiftx],[0,1, shifty]])
-    rows,cols = img.shape
-    dst = cv2.warpAffine(img, M, (cols,rows))
-    working_img = cv2.warpAffine(working_img, M, (cols,rows))
-    display_img = cv2.warpAffine(display_img, M, (cols,rows))
+    M = numpy.float32([[1, 0, shiftx], [0, 1, shifty]])
+    rows, cols = img.shape
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    working_img = cv2.warpAffine(working_img, M, (cols, rows))
+    display_img = cv2.warpAffine(display_img, M, (cols, rows))
 
     x_range = x_maxline[0] - x_minline[0]
     y_range = y_maxline[1] - y_minline[1]
 
-    print("vertical gradient 1: {} angle: {} ".format(m_vert_1, math.atan(m_vert_1)))
-    print("vertical gradient 2: {} angle: {} ".format(m_vert_2, math.atan(m_vert_2)))
-    print("horizontal gradient 1: {} angle: {} ".format(m_hor_1, math.atan(m_hor_1)))
-    print("horizontal gradient 2: {} angle: {} ".format(m_hor_2, math.atan(m_hor_2)))
+    print("vertical gradient 1: {} angle: {} ".format(
+        m_vert_1, math.atan(m_vert_1)))
+    print("vertical gradient 2: {} angle: {} ".format(
+        m_vert_2, math.atan(m_vert_2)))
+    print("horizontal gradient 1: {} angle: {} ".format(
+        m_hor_1, math.atan(m_hor_1)))
+    print("horizontal gradient 2: {} angle: {} ".format(
+        m_hor_2, math.atan(m_hor_2)))
     # TODO rotation
 
-    # offset for first (first column in first row) box 
+    # offset for first (first column in first row) box
     x_offset = x_range * 0.505
     y_offset = y_range * 0.235
     # box size
-    xsize = x_range  * 0.05  
-    ysize = y_range  * 0.04
+    xsize = x_range * 0.05
+    ysize = y_range * 0.04
     # distance between to boxes
-    xdiff = xsize 
+    xdiff = xsize
     ydiff = y_range * 0.0625
 
     maxValues = {}
     if DEBUG:
         print("xrange: {} yrange {} ".format(x_range, y_range))
 
-    # get non blured bw image 
-    tmp, non_blured_bw_img = cv2.threshold(img, 0, 255, 
-            cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU )
-    #translate this image
-    non_blured_bw_img = cv2.warpAffine(non_blured_bw_img, M, (cols,rows))
+    # get non blured bw image
+    tmp, non_blured_bw_img = cv2.threshold(
+            img, 0, 255,
+            cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # translate this image
+    non_blured_bw_img = cv2.warpAffine(non_blured_bw_img, M, (cols, rows))
 
     for row in range(0, 12):
         maxValues[row] = (-1, 0)
@@ -190,8 +200,6 @@ if __name__ == "__main__":
     VISUAL_DEBUG = True
     DEBUG = True
     imageName = "../testimages/card__859724459716.jpg"
-  #  imageName = "../testimages/card_webcam__898656717575.jpg"
+    #  imageName = "../testimages/card_webcam__898656717575.jpg"
     print(getVotesFromImage(imageName))
     print(imageName)
-
-
